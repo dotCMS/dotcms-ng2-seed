aa8245a5ebc400b0494bf1ffd206cc4175f57f54
Freddy Montes <freddymontes@gmail.com>
2016-10-21 13:28:06 -0600

    Basic dotcms-js config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
+import {JWTAuthService} from 'dotcms-js/core/util/jwt-auth.service';
 
 @Component({
+  encapsulation: ViewEncapsulation.None,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [
+    './app.component.css',
+    '../../node_modules/primeng/resources/primeng.min.css',
+    '../../node_modules/primeng/resources/themes/omega/theme.css'
+  ]
 })
 export class AppComponent {
   title = 'app works!';
+
+  constructor(private jWTAuthService: JWTAuthService) {
+    jWTAuthService.login('http://demo37.dotcms.com', 'admin@dotcms.com', 'admin').subscribe(res => {
+      console.log('Login Successfully');
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {Logger} from "angular2-logger/core";
 
 import { AppComponent } from './app.component';
+
+// Import all the services
+import {
+  AppConfig,
+  FileSystemService,
+  HttpClient,
+  JWTAuthService,
+  LocalStoreService,
+  LoggerService,
+  NotificationService,
+  SettingsStorageService,
+  SiteBrowserService,
+  SiteBrowserState,
+  SiteSelectorService,
+  SiteTreetableService,
+} from 'dotcms-js/dotcms-js';
+
+// Import all modules
+import {
+  DotcmsBreadcrumbModule,
+  DotcmsSiteDatatableModule,
+  DotcmsSiteSelectorModule,
+  DotcmsSiteTreeTableModule,
+  DotcmsTreeableDetailModule,
+} from 'dotcms-js/dotcms-js';
 
 @NgModule({
   declarations: [
@@ -12,9 +38,29 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+
+    DotcmsBreadcrumbModule,
+    DotcmsSiteDatatableModule,
+    DotcmsSiteSelectorModule,
+    DotcmsSiteTreeTableModule,
+    DotcmsTreeableDetailModule,
   ],
-  providers: [],
+  providers: [
+    Logger,
+    {provide: AppConfig, useClass: AppConfig},
+    {provide: FileSystemService, useClass: FileSystemService},
+    {provide: HttpClient, useClass: HttpClient},
+    {provide: JWTAuthService, useClass: JWTAuthService},
+    {provide: LocalStoreService, useClass: LocalStoreService},
+    {provide: LoggerService, useClass: LoggerService},
+    {provide: NotificationService, useClass: NotificationService},
+    {provide: SettingsStorageService, useClass: SettingsStorageService},
+    {provide: SiteBrowserService, useClass: SiteBrowserService},
+    {provide: SiteBrowserState, useClass: SiteBrowserState},
+    {provide: SiteSelectorService, useClass: SiteSelectorService},
+    {provide: SiteTreetableService, useClass: SiteTreetableService},
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
